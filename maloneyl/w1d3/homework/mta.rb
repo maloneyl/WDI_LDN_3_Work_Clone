a246d2097905c32d8bcb1fc9b163d35fe65d9690
maloneyl <m@thinkmaloney.com>
2013-10-02 22:18:21 +0100

    refactored for readability and ease of user input (enter number instead of name)

--- maloneyl/w1d3/homework/mta.rb
+++ maloneyl/w1d3/homework/mta.rb
@@ -4,61 +4,74 @@
 # As all lines intersect at Union Square, distance between anything cross-lines should be
 # number of stops from Union Sq on its own line + number of stops from Union Sq on the other line
 
-# Guess we can calculate distances between the same line by getting the stop through index, to_i, then subtract
+# If to-from are on the same line, then the distance is just their index differences
 
+# For section readability
+require 'rainbow'
 
+# Our 3 lines
 line_n = [ "Times Square", "34th", "28th", "23rd", "Union Square", "8th" ]
 line_l = [ "8th", "6th", "Union Square", "3rd", "1st" ]
 line_6 = [ "Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place" ]
 
-puts "FROM:"
-puts "Line: N, L or 6"
-from_line = gets.chomp
+puts "***** JOURNEY PLANNER *****"
 
-case from_line
+# Ask and store user input re: origin
+puts "\nI'M STARTING AT...".color(:blue)
+puts "\nLINE: Enter N, L or 6"
+from_line = gets.chomp.strip
+puts "\nSTATION: Enter by number below"
+case from_line 
 when "n", "N"
-  puts "Stop: Times Square, 34th, 28th, 23rd, Union Square, 8th"
-  from_line_base = line_n
+  line_n.each_with_index { |station, index| puts "#{index+1}. #{station}" } # +1 because starting from 0 is weird to normal people
+  origin_line_to_use = line_n
 when "l", "L"
-  puts "Stop: 8th, 6th, Union Square, 3rd, 1st"
-  from_line_base = line_l
+  line_l.each_with_index { |station, index| puts "#{index+1}. #{station}" } 
+  origin_line_to_use = line_l
 when "6"
-  puts "Stop: Grand Central, 33rd, 28th, 23rd, Union Square, Astor Place"
-  from_line_base = line_6
+  line_6.each_with_index { |station, index| puts "#{index+1}. #{station}" } 
+  origin_line_to_use = line_6
 end
+from_stop = gets.chomp.to_i
 
-from_stop = gets.chomp 
-
-puts "TO:"
-puts "Line: N, L or 6"
-to_line = gets.chomp
-
-case to_line
+# Ask and store user input re: destination
+puts "\nI'M GOING TO...".color(:magenta)
+puts "\nLINE: Enter N, L or 6"
+to_line = gets.chomp.strip
+puts "\nSTATION: Enter by number below"
+case to_line 
 when "n", "N"
-  puts "Stop: Times Square, 34th, 28th, 23rd, Union Square, 8th"
-  to_line_base = line_n
+  line_n.each_with_index { |station, index| puts "#{index+1}. #{station}" }
+  destination_line_to_use = line_n
 when "l", "L"
-  puts "Stop: 8th, 6th, Union Square, 3rd, 1st"
-  to_line_base = line_l  
+  line_l.each_with_index { |station, index| puts "#{index+1}. #{station}" } 
+  destination_line_to_use = line_l
 when "6"
-  puts "Stop: Grand Central, 33rd, 28th, 23rd, Union Square, Astor Place"
-  to_line_base = line_6
+  line_6.each_with_index { |station, index| puts "#{index+1}. #{station}" } 
+  destination_line_to_use = line_6
 end
+to_stop = gets.chomp.to_i
 
-to_stop = gets.chomp 
-
-# If to-from are on the same line, the distance is just the index difference in the array.
-if from_line == to_line
-   distance = from_line_base.index(from_stop) - to_line_base.index(to_stop)
-   puts "DISTANCE: #{distance.abs} stops"
-# If it is not, count the number of stops between from_stop and Union Square
-# as well as the number of stops between the to_stop from Union Square
+# If to-from are on the same line, the distance is just the index difference in the array, made absolute
+if origin_line_to_use == destination_line_to_use
+   distance = (from_stop - to_stop).abs
+   if distance > 1
+    puts "\nDISTANCE: #{distance} stops".color(:green)
+   else 
+    puts "\nDISTANCE: #{distance} stop".color(:green)
+   end
+# If to-from are on different lines, count the number of stops between 'from' and Union Square
+# as well as the number of stops between 'to' and Union Square
 else
-  from_dist_from_u_sq = from_line_base.index(from_stop) - from_line_base.index("Union Square")
+  origin_dist_from_u_sq = (from_stop-1) - origin_line_to_use.index("Union Square")
   # puts from_dist_from_u_sq # just testing
-  to_dist_from_u_sq = to_line_base.index(to_stop) - to_line_base.index("Union Square")
+  destination_dist_from_u_sq = (to_stop-1) - destination_line_to_use.index("Union Square")
   # puts to_dist_from_u_sq # just testing
-  distance = from_dist_from_u_sq.abs + to_dist_from_u_sq.abs
-  puts "DISTANCE: #{distance} stops"
+  distance = origin_dist_from_u_sq.abs + destination_dist_from_u_sq.abs
+  if distance > 1
+    puts "\nDISTANCE: #{distance} stops".color(:green)
+  else 
+    puts "\nDISTANCE: #{distance} stop".color(:green)
+  end
 end
 
