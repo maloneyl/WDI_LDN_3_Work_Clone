b7372c0b140dc9783693393c9f74d498c10cf665
maloneyl <m@thinkmaloney.com>
2013-10-22 09:54:33 +0100

    added library model

--- maloneyl/w4d1/homework/library_app/app/controllers/libraries_controller.rb
+++ maloneyl/w4d1/homework/library_app/app/controllers/libraries_controller.rb
@@ -1,3 +1,40 @@
 class LibrariesController < ApplicationController
 
+  def index
+    @libraries = Library.all
+  end
+
+  def new
+    @library = Library.new
+  end
+
+  def create
+    @library = Library.new params[:library]
+    if @library.save
+      redirect_to @library
+    else
+      render :new
+    end
+  end
+
+  def show
+    @library = Library.find params[:id]
+  end
+
+  def edit
+    @library = Library.find params[:id]
+  end
+
+  def update
+    @library = Library.find params[:id]
+    @library.update_attributes params[:library]
+    redirect_to @library
+  end
+
+  def destroy
+    library = Library.find params[:id]
+    library.delete
+    redirect_to libraries_path
+  end
+
 end
