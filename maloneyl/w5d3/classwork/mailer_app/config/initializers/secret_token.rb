7594136f14136390d7825771fd0421162a0be1ed
maloneyl <m@thinkmaloney.com>
2013-10-30 13:09:21 +0000

    added mailer_app to learn how to use actionmailer and mandrill

---
+++ maloneyl/w5d3/classwork/mailer_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailerApp::Application.config.secret_token = 'c0ce61d4dccdb046088a3529a2a80222d7f1d41577ddb1450517c7b3b96c2766c41f97b526f52f794a722af83215ebdb2501ea81dbe4a99343886b707a731f8b'
