4acd76570c2084e61154bdf0f5aeb716d71e51f1
maloneyl <m@thinkmaloney.com>
2013-11-13 09:47:30 +0000

    left file open in sublime, pretty sure nothing changed

--- maloneyl/w7d2/classwork/gallery_app_omniauth_start/config/initializers/devise.rb
+++ maloneyl/w7d2/classwork/gallery_app_omniauth_start/config/initializers/devise.rb
@@ -251,5 +251,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :google_oauth2, GOOG_OAUTH_ML_CLIENT_ID, GOOG_OAUTH_ML_CLIENT_SECRET # client id and client secret
+  config.omniauth :google_oauth2, ENV["GOOG_OAUTH_ML_CLIENT_ID"], ENV["GOOG_OAUTH_ML_CLIENT_SECRET"]
 end
